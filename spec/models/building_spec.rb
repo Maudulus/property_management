57711c55000b1cae6dfabe245b0118fc5098234b
Eric Kelly <heroiceric@gmail.com>
2014-04-07 17:49:13 -0400

    Refactor unit tests

--- spec/models/building_spec.rb
+++ spec/models/building_spec.rb
@@ -2,18 +2,16 @@ require 'spec_helper'
 
 describe Building do
   it { should have_valid(:address).when('200 Dane Street') }
-  it { should_not have_valid(:address).when('') }
+  it { should_not have_valid(:address).when(nil, '') }
 
   it { should have_valid(:city).when('MA') }
-  it { should_not have_valid(:city).when(123) }
-  it { should_not have_valid(:city).when("") }
+  it { should_not have_valid(:city).when(nil, '', 123) }
 
   it { should have_valid(:state).when('WY') }
-  it { should_not have_valid(:state).when("BC") }
-  it { should_not have_valid(:state).when(123) }
+  it { should_not have_valid(:state).when(nil, '', "BC", 123) }
 
   it { should have_valid(:zip).when(62445) }
+  it { should_not have_valid(:zip).when(nil, '') }
 
-  it { should belong_to :owner}
-
+  it { should belong_to :owner }
 end
