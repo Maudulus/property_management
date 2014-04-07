146ea6ef4211c9e4e7aec7980905cee4365a89f9
Eric Kelly <heroiceric@gmail.com>
2014-04-07 18:03:03 -0400

    Refactor owner model

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,7 +1,9 @@
 class Owner < ActiveRecord::Base
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
   has_many :buildings, dependent: :nullify
 
-  validates :first_name, presence: true, format: { with: /[a-zA-Z]/, on: :create }
-  validates :last_name, presence: true, format: { with: /[a-zA-Z]/, on: :create }
-  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :email, presence: true, format: { with: EMAIL_REGEX }
 end
